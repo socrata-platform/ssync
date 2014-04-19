2a54bf009c8a649ce98473275e219019f0bbda3b
Robert J. Macomber <robert.macomber@socrata.com>
2014-04-19 11:04:10 -0700

    Rework the patch computer to allow a pull version

--- src/main/java/com/socrata/ssync/PatchComputer.java
+++ src/main/java/com/socrata/ssync/PatchComputer.java
@@ -11,7 +11,15 @@ public class PatchComputer {
     private final RollingChecksum rc;
     private final byte[] buffer;
     private int dataStart;
+    private int readPtr; // Invariant : 0 <= dataStart <= readPtr <= bufferEnd <= buffer.length
     private int bufferEnd;
+    private int state;
+    private boolean sawEOF;
+    static final int StateNewBlock = 0;
+    static final int StateContinueBlock = 1;
+    static final int StateNewPartialBlock = 2;
+    static final int StateContinuePartialBlock = 3;
+    static final int StateDone = 4;
 
     private PatchComputer(InputStream in, SignatureTable signatureTable, String checksumAlgorithm, int maxMemory, OutputStream out)
             throws IOException, NoSuchAlgorithmException
@@ -25,153 +33,173 @@ public class PatchComputer {
             throw new IllegalArgumentException("Cannot store at least two blocks given the max memory limit");
         }
         rc = new RollingChecksum(blockSize);
+        readPtr = dataStart = bufferEnd = 0;
+        sawEOF = false;
+    }
+
+    private void step() throws IOException {
+        boolean wrote;
+        do {
+            switch(state) {
+                case StateNewBlock: wrote = newBlock(); break;
+                case StateContinueBlock: wrote = continueBlock(); break;
+                case StateNewPartialBlock: wrote = newPartialBlock(); break;
+                case StateContinuePartialBlock: wrote = continuePartialBlock(); break;
+                case StateDone: return;
+                default: throw new RuntimeException("Unknown state " + state);
+            }
+        } while(!wrote);
+    }
+
+    private int remaining() {
+        return bufferEnd - readPtr;
+    }
+
+    private boolean hasFullBlock() {
+        return remaining() >= blockSize;
+    }
+
+    private boolean newBlock() throws IOException {
+        if(maybeFlush()) return true;
+        if(!hasFullBlock()) {
+            refillBuffer();
+            if(!hasFullBlock()) {
+                state = StateNewPartialBlock;
+                return newPartialBlock();
+            }
+        }
+        int weakHash = rc.forBlock(buffer, readPtr, blockSize);
+        int blockNum = signatureTable.findBlock(weakHash, buffer, readPtr, blockSize);
+        if(blockNum != -1) {
+            writeBlock(blockNum, blockSize);
+            return true;
+        }
+        readPtr += 1;
+        state = StateContinueBlock;
+        return continueBlock();
+    }
+
+    private boolean newPartialBlock() throws IOException {
+        if(maybeFlush()) return true;
+        int partialBlockSize = remaining();
+        if(partialBlockSize > 0) {
+            int weakHash = rc.forBlock(buffer, readPtr, partialBlockSize);
+            int blockNum = signatureTable.findBlock(weakHash, buffer, readPtr, partialBlockSize);
+            if(blockNum != -1) {
+                writeBlock(blockNum, partialBlockSize);
+                state = StateDone;
+                return true;
+            }
+            readPtr += 1;
+            state = StateContinueBlock;
+            return continuePartialBlock();
+        } else {
+            state = StateDone;
+            return false;
+        }
+    }
+
+    // precondition: readPtr > 0
+    private boolean continueBlock() throws IOException {
+        byte lastByte = buffer[readPtr - 1];
+        while(true) {
+            if(maybeFlush()) return true;
+            if(!hasFullBlock()) {
+                refillBuffer();
+                if(!hasFullBlock()) {
+                    state = StateContinuePartialBlock;
+                    return continueContinuePartialBlock(lastByte);
+                }
+            }
+            int weakHash = rc.roll(lastByte, buffer[readPtr + blockSize - 1]);
+            int blockNum = signatureTable.findBlock(weakHash, buffer, readPtr, blockSize);
+            if(blockNum != -1) {
+                writeBlock(blockNum, blockSize);
+                state = StateNewBlock;
+                return true;
+            }
+            lastByte = buffer[readPtr];
+            readPtr += 1;
+        }
+    }
+
+    // precondition: readPtr > 0
+    private boolean continuePartialBlock() throws IOException {
+        byte lastByte = buffer[readPtr - 1];
+        return continueContinuePartialBlock(lastByte);
+    }
+
+    private boolean continueContinuePartialBlock(byte lastByte) throws IOException {
+        while(readPtr < bufferEnd) {
+            if(maybeFlush()) return true;
+            int weakHash = rc.roll(lastByte, (byte) 0);
+            int blockNum = signatureTable.findBlock(weakHash, buffer, readPtr, remaining());
+            if(blockNum != -1) {
+                writeBlock(blockNum, remaining());
+                state = StateDone;
+                return true;
+            }
+            lastByte = buffer[readPtr];
+            readPtr += 1;
+        }
+        state = StateDone;
+        return false;
     }
 
     private void go() throws IOException {
-        fillBuffer();
-        int startPos = 0;
-        while(true) {
-            if(startPos < 0) { // consumed the whole buffer without finding any blocks
-                startPos = refillBuffer(-startPos);
-                if(bufferEnd - startPos < blockSize) break; // no more whole blocks to be read
-                startPos = continueScan(startPos);
-            } else {
-                if(bufferEnd - startPos < blockSize) { // need to make room for more block data
-                    startPos = refillBuffer(startPos);
-                    if(bufferEnd - startPos < blockSize) break; // there still isn't a full block!
-                }
-                startPos = restartScan(startPos);
-            }
-        }
-
-        if(startPos < 0) {
-            startPos = (-startPos) - compressBuffer();
-            continueScanPartial(startPos);
-        } else {
-            startPos -= compressBuffer();
-            restartScanPartial(startPos);
-        }
-
-        writeData(dataStart, bufferEnd);
+        readIntoBuffer();
+        while(state != StateDone) { step(); }
+        flush();
         patch.writeEnd();
     }
 
-    // precond: a full block is present in the buffer.
-    // returns the position of the first byte after a found block, or the negative
-    // of the point at which it stopped scanning.
-    // Scans whole blocks only, not potential partial blocks.
-    private int restartScan(int from) throws IOException {
-        maybeFlush(from);
-        int weakHash = rc.forBlock(buffer, from, blockSize);
-        int blockNum = signatureTable.findBlock(weakHash, buffer, from, blockSize);
-        if(blockNum != -1) {
-            return writeBlock(from, blockNum, blockSize);
-        }
-        return continueScan(from + 1);
-    }
+    private void readIntoBuffer() throws IOException {
+        if(sawEOF) return;
 
-    // precond: a full block is NOT present in the buffer
-    private void restartScanPartial(int from) throws IOException {
-        maybeFlush(from);
-        int remaining = bufferEnd - from;
-        if(remaining != 0) {
-            int weakHash = rc.forBlock(buffer, from, remaining);
-            int blockNum = signatureTable.findBlock(weakHash, buffer, from, remaining);
-            if(blockNum != -1) {
-                writeBlock(from, blockNum, remaining);
-            } else {
-                continueScanPartial(from + 1);
-            }
-        }
-    }
-
-    // precond: from is at least 1.
-    // returns the position of the first byte after a found block, or the negative
-    // of the point at which it stopped scanning.
-    // Scans whole blocks only, not potential partial blocks.
-    private int continueScan(int from) throws IOException {
-        while(from <= bufferEnd - blockSize) {
-            maybeFlush(from);
-            int weakHash = rc.roll(buffer[from - 1], buffer[from + blockSize - 1]);
-            int blockNum = signatureTable.findBlock(weakHash, buffer, from, bufferEnd - from);
-            if(blockNum != -1) {
-                return writeBlock(from, blockNum, blockSize);
-            }
-            from += 1;
-        }
-        return -from;
-    }
-
-    // precond: from is at least 1.
-    private void continueScanPartial(int from) throws IOException {
-        while(from < bufferEnd) {
-            maybeFlush(from);
-            int weakHash = rc.roll(buffer[from - 1], (byte) 0);
-            int blockNum = signatureTable.findBlock(weakHash, buffer, from, bufferEnd);
-            if(blockNum != -1) {
-                writeBlock(from, blockNum, bufferEnd - from);
-                return;
-            }
-            from += 1;
-        }
-    }
-
-    // initializes dataStart, bufferEnd, and buffer.
-    private void fillBuffer() throws IOException {
-        int count = readIntoBuffer(0);
-        dataStart = 0;
-        bufferEnd = count;
-    }
-
-    private int readIntoBuffer(int off) throws IOException {
-        int total = 0;
+        int off = bufferEnd;
         int len = buffer.length - off;
         while(len > 0) {
             int code = in.read(buffer, off, len);
-            if(code == -1) break;
-            total += code;
+            if(code == -1) { sawEOF = true; break; }
             off += code;
             len -= code;
         }
-        return total;
+        bufferEnd = off;
     }
 
-    // updates dataStart, bufferEnd, and buffer; returns the new startPos.
+    // updates readPtr, dataStart, bufferEnd, sawEOF.
     // After this, dataStart is 0.
-    int refillBuffer(int startPos) throws IOException {
-        int shifted = compressBuffer();
-        int kept = bufferEnd - shifted;
-        bufferEnd = kept + readIntoBuffer(kept);
-        return startPos - shifted;
+    private void refillBuffer() throws IOException {
+        if(sawEOF) return;
+        compressBuffer();
+        readIntoBuffer();
     }
 
-    int compressBuffer() {
-        int toKeep = bufferEnd - dataStart;
-        int toShift = dataStart;
+    private void compressBuffer() {
+        int shiftAmount = dataStart;
+        int toKeep = bufferEnd - shiftAmount;
         System.arraycopy(buffer, dataStart, buffer, 0, toKeep);
         dataStart = 0;
-        return toShift;
+        readPtr -= shiftAmount;
+        bufferEnd -= shiftAmount;
     }
 
-    private int writeBlock(int from, int blockNum, int blockSize) throws IOException {
-        writeData(dataStart, from);
+    private void writeBlock(int blockNum, int blockSize) throws IOException {
+        flush();
         patch.writeBlockNum(blockNum);
-        int newFrom = from + blockSize;
-        dataStart = newFrom;
-        return newFrom;
+        dataStart += blockSize;
+        readPtr += blockSize;
     }
 
-    private void maybeFlush(int dataEnd) throws IOException {
-        int count = dataEnd - dataStart;
-        if(count == blockSize) {
-            writeData(dataStart, dataEnd);
-            dataStart = dataEnd;
-        }
+    private boolean maybeFlush() throws IOException {
+        if(readPtr - dataStart != blockSize) return false;
+        flush();
+        return true;
     }
 
-    private void writeData(int dataStart, int dataEnd) throws IOException {
-        int count = dataEnd - dataStart;
-        patch.writeData(buffer, dataStart, count);
+    private void flush() throws IOException {
+        patch.writeData(buffer, dataStart, readPtr - dataStart);
+        dataStart = readPtr;
     }
 
     public static void compute(InputStream in, SignatureTable signatureTable, String checksumAlgorithm, int maxMemory, OutputStream out)
