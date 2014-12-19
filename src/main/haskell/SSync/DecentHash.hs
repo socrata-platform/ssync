7fdaec697004bb248b6e29e263b2fe4a47ebe1f1
Robert J. Macomber <robert.macomber@socrata.com>
2014-12-19 10:12:13 -0800

    Let Hash be implemented by either Crypto or cryptohash
    cryptohash is way faster, but not pure Haskell so it
    can't be used with ghcjs

---
+++ src/main/haskell/SSync/DecentHash.hs
@@ -0,0 +1,60 @@
+{-# LANGUAGE RankNTypes, ScopedTypeVariables, RecordWildCards, NamedFieldPuns, DeriveDataTypeable, OverloadedStrings, InstanceSigs #-}
+
+module SSync.DecentHash (
+  withHashM
+, HashT
+, update
+, digest
+, digestSize
+, NoSuchHashAlgorithm(..)
+) where
+
+import qualified Crypto.Hash as C
+import qualified Data.ByteString as BS
+import Data.Byteable (toBytes)
+import Control.Monad.State.Strict
+
+import Control.Exception
+import Data.Typeable
+import Data.Text (Text)
+
+data NoSuchHashAlgorithm = NoSuchHashAlgorithm Text deriving (Show, Typeable)
+
+instance Exception NoSuchHashAlgorithm
+
+type HashT m b = forall a. C.HashAlgorithm a => StateT (C.Context a) m b
+
+withHashM :: (Monad m) => Text -> HashT m b -> m b
+withHashM alg op = withHashM' alg $ \ctx -> flip evalStateT ctx op
+
+-- | Feed the current hash with some bytes.
+update :: (Monad m) => BS.ByteString -> HashT m ()
+update bs = modify (flip C.hashUpdate bs)
+
+-- | Returns the current value of the hash.
+digest :: (Monad m) => HashT m BS.ByteString
+digest = do
+  ctx <- get
+  return $ toBytes $ C.hashFinalize ctx
+
+-- | Returns the size of the current hash's digest, in bytes.  Note:
+-- this is not a fast operation.
+digestSize :: (Monad m) => (forall a. C.HashAlgorithm a => StateT (C.Context a) m Int)
+digestSize = do
+  d <- digest
+  return $ BS.length d
+
+withHashM' :: (Monad m) => Text -> (forall a. C.HashAlgorithm a => C.Context a -> m b) -> m b
+withHashM' "MD4" f = withMD4 f
+withHashM' "MD5" f = withMD5 f
+withHashM' "SHA1" f = withSHA1 f
+withHashM' "SHA256" f = withSHA256 f
+withHashM' "SHA512" f = withSHA512 f
+withHashM' other _ = throw $ NoSuchHashAlgorithm other
+
+withMD4, withMD5, withSHA1, withSHA256, withSHA512 :: (Monad m) => (forall a. C.HashAlgorithm a => C.Context a -> m b) -> m b
+withMD4 f = f (C.hashInit :: C.Context C.MD4)
+withMD5 f = f (C.hashInit :: C.Context C.MD5)
+withSHA1 f = f (C.hashInit :: C.Context C.SHA1)
+withSHA256 f = f (C.hashInit :: C.Context C.SHA256)
+withSHA512 f = f (C.hashInit :: C.Context C.SHA512)
---
+++ src/main/haskell/SSync/StupidHash.hs
@@ -0,0 +1,68 @@
+{-# LANGUAGE RankNTypes, ScopedTypeVariables, RecordWildCards, NamedFieldPuns, DeriveDataTypeable, OverloadedStrings, InstanceSigs #-}
+
+module SSync.StupidHash (
+  withHashM
+, HashT
+, update
+, digest
+, digestSize
+, NoSuchHashAlgorithm(..)
+) where
+
+import qualified Data.Digest.MD5 as MD5
+import qualified Data.Digest.SHA1 as SHA1
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as BSL
+import Data.ByteString (ByteString)
+import Control.Monad.State.Strict
+
+import Control.Exception
+import Data.Typeable
+import Data.Text (Text)
+import Data.Word (Word8, Word32)
+import qualified Data.DList as DL
+import Data.Bits (shiftR)
+
+data NoSuchHashAlgorithm = NoSuchHashAlgorithm Text deriving (Show, Typeable)
+
+instance Exception NoSuchHashAlgorithm
+
+type S = ([Word8] -> ByteString, (DL.DList ByteString))
+
+type HashT m = StateT S m
+
+withHashM :: (Monad m) => Text -> HashT m b -> m b
+withHashM alg op = flip evalStateT (hasher alg, DL.empty) op
+
+-- | Feed the current hash with some bytes.
+update :: (Monad m) => BS.ByteString -> HashT m ()
+update bs = modify (addBytes bs)
+
+addBytes :: ByteString -> S -> S
+addBytes bs (f, dl) = (f, DL.snoc dl bs)
+
+-- | Returns the current value of the hash.
+digest :: (Monad m) => HashT m BS.ByteString
+digest = do
+  (f, bytes') <- get
+  let bytes = BSL.unpack $ BSL.fromChunks $ DL.toList bytes'
+  return $ f bytes
+
+-- | Returns the size of the current hash's digest, in bytes.  Note:
+-- this is not a fast operation.
+digestSize :: (Monad m) => (HashT m Int)
+digestSize = do
+  d <- digest
+  return $ BS.length d
+
+hasher :: Text -> [Word8] -> ByteString
+hasher "MD5" = BS.pack . MD5.hash
+hasher "SHA1" = shaRes . SHA1.hash
+hasher other = throw $ NoSuchHashAlgorithm other
+
+shaRes :: SHA1.Word160 -> ByteString
+shaRes (SHA1.Word160 a b c d e) =
+  BS.pack $ buildWord a $ buildWord b $ buildWord c $ buildWord d $ buildWord e []
+
+buildWord :: Word32 -> [Word8] -> [Word8]
+buildWord w tl = (fromIntegral $ w `shiftR` 24) : (fromIntegral $ w `shiftR` 16) : (fromIntegral $ w `shiftR` 8) : fromIntegral w : tl
