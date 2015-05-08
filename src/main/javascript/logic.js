33a69c8f5bcc63a10aab20f144a84fd6ccaa5479
Robert J. Macomber <robert.macomber@socrata.com>
2015-05-08 15:02:24 -0700

    Clean up the UI a bit

--- src/main/javascript/logic.js
+++ src/main/javascript/logic.js
@@ -3,23 +3,45 @@ var ssyncleton = new DatasyncUpload({
     forceWorkerReload : true
 });
 
-function go2(controlFileRaw) {
+var controlFileEditor = undefined;
+
+$(document).ready(function() {
+    var ff = localStorage.getItem("fourfour");
+    if(ff !== null) $("#fourfour").val(ff);
+
+    controlFileEditor = ace.edit("control");
+    var editorSession = controlFileEditor.getSession();
+    editorSession.setMode("ace/mode/json");
+    editorSession.setTabSize(2);
+    editorSession.setUseSoftTabs(true);
+
+    var cf = localStorage.getItem("control");
+    if(cf !== null) {
+        controlFileEditor.setValue(cf, -1);
+    }
+});
+
+function go() {
     function msg(s) {
         $("#message").text(s);
     };
 
-    function ds_status(s) {
-        $("#status").text(s);
+    function ds_log(s) {
+        $("#log").text(s);
     };
 
-    function ds_log(s) {
-        $("#log").text(s); // append(document.createTextNode(s));
-    };
     function alertJS(x) {
         alert(JSON.stringify(x));
     };
+
+    var ff = $("#fourfour").val();
     var file = $("#input")[0].files[0];
-    var datasetId = $("#fourfour").val();
+    if(!file) {
+        msg("No file specified");
+        return;
+    }
+    var controlFileRaw = controlFileEditor.getValue();
+
     function progress(prog) {
         if(prog.stage == ssyncleton.UploadingData) msg("read " + prog.file_bytes_read + " bytes ("+ (100*prog.file_bytes_read/prog.file_bytes_total).toFixed(3) + "%); sent " + prog.bytes_uploaded + " bytes");
         else if(prog.stage == ssyncleton.WatchingProgress) {
@@ -34,8 +56,14 @@ function go2(controlFileRaw) {
             ds_log(log.join("\n"));
         }
     };
-    var onError = alertJS;
-    function onComplete(x) { progress(x); alert("finished!\n" + JSON.stringify(x)) };
+    function onError(msg) {
+        $("#go").prop("disabled", false);
+        alertJS(msg);
+    }
+    function onComplete(x) {
+        progress(x);
+        $("#go").prop("disabled", false);
+    };
     headers = {
         "x-csrf-token" : encodeURIComponent($.cookie('socrata-csrf-token')),
         "x-app-token" : $("#apptoken").val()
@@ -50,9 +78,10 @@ function go2(controlFileRaw) {
         }
         throw e;
     }
-    localStorage.setItem("fourfour", datasetId);
+    localStorage.setItem("fourfour", ff);
     localStorage.setItem("control", controlFileRaw);
-    ssyncleton.upload(datasetId, controlFile, file,
+    $("#go").prop("disabled", true);
+    ssyncleton.upload(ff, controlFile, file,
                       { progress : progress,
                         onError : onError,
                         onComplete : onComplete,
