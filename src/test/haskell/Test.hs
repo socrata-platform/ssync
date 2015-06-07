54fb18245fb371294c296bcac5419367d36a6a68
Robert J. Macomber <robert.macomber@socrata.com>
2015-06-07 16:02:44 -0700

    Use tasty instead of raw quickcheck
    Adds flexibility, and most importantly reports repro information when a
    quickcheck test fails.

--- src/test/haskell/Test.hs
+++ src/test/haskell/Test.hs
@@ -1,19 +1,15 @@
 {-# OPTIONS_GHC -fno-warn-orphans #-}
-{-# LANGUAGE TemplateHaskell #-}
 
-module Main (
-  main
-) where
+import Test.Tasty
+import Test.Tasty.QuickCheck (testProperty, Arbitrary, arbitrary, elements, frequency, choose, shrink)
 
 import Conduit
 import Control.Applicative ((<$>))
-import System.Exit (exitSuccess, exitFailure)
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as BS
 import Data.Maybe (mapMaybe)
 import Data.Foldable (foldMap)
 import Data.Monoid (Sum(..))
-import Test.QuickCheck (quickCheckAll, Arbitrary, arbitrary, shrink, elements, choose, frequency)
 import Test.QuickCheck.Instances ()
 
 import SSync.SignatureComputer
@@ -27,16 +23,18 @@ instance Arbitrary BlockSize where
                                                       ]
   shrink bs = mapMaybe (blockSize . fromIntegral) (shrink $ blockSizeWord bs)
 
-prop_sigLen :: HashAlgorithm -> HashAlgorithm -> BlockSize -> [ByteString] -> Bool
-prop_sigLen chk strong bs bytes =
-  let lenSum = Sum . fromIntegral . BS.length
-      realLen = getSum . runIdentity $ yieldMany bytes $$ produceSignatureTable chk strong bs $= foldMapC lenSum
-      estLen = signatureTableSize chk strong bs (getSum $ foldMap lenSum bytes)
-  in realLen == estLen
-
-return []
-
 main :: IO ()
-main = do
-  success <- $quickCheckAll
-  if success then exitSuccess else exitFailure
+main = defaultMain tests
+
+tests :: TestTree
+tests = testSignatureTableSize
+
+testSignatureTableSize :: TestTree
+testSignatureTableSize =
+  testProperty "length (computeSignatureTable bytes) == signatureTableSize bytes" prop
+  where prop :: HashAlgorithm -> HashAlgorithm -> BlockSize -> [ByteString] -> Bool
+        prop chk strong bs bytes =
+          let lenSum = Sum . fromIntegral . BS.length
+              realLen = getSum . runIdentity $ yieldMany bytes $$ produceSignatureTable chk strong bs $= foldMapC lenSum
+              estLen = signatureTableSize chk strong bs (getSum $ foldMap lenSum bytes)
+          in realLen == estLen
